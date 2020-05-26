09a1fd95e75c5e31eda5aeae268cbafead5aca3a
Raphaël Barrois <raphael.barrois@polytechnique.org>
2020-05-26 10:26:20 +0200

    Add support for Python 3.8.
    No code updates required.

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     test_suite='',
     test_loader='unittest:TestLoader',
